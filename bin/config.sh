0ee0d7bbd14ebf0227d49574bb7218052d2f7d86
qinweijunlin <qinweijunlin@qq.com>
2019-04-06 13:32:29 +0800

    发布

--- bin/config.sh
+++ bin/config.sh
@@ -1,32 +1,30 @@
 #!/bin/bash
 #
-# 控制手机的起点
+# 获取配置文件的值
 set -euo pipefail
 
-#### 常量 ####
-readonly ROOTDIR='/yzaj'
+# USAGE: config.sh 键 值 文件
 
-#### 函数 ####
+readonly E_MISSING_OPERAND=1
+readonly E_NOT_FOUND=2
+
 err() {
   echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
 }
 
-#### 变量 ####
-readonly bindir="${ROOTDIR}/phone/bin"
-readonly confdir="${ROOTDIR}/phone/conf"
-readonly tempdir="${ROOTDIR}/temp/phone"
-todaydir="${tempdir}/log/$(date +'%Y-%m-%d')"
-beforedir="${tempdir}/log/$(date +'%Y-%m-%d' -d '-7day')"
-readonly todaydir
-readonly beforedir
-phonelog="${todaydir}/$(date +'%H-%M-%S').log"
-readonly phonelog
+readonly key="$1"
+readonly value="$2"
+readonly filename="$3"
 
-#### 主体 ####
-if [[ -d "${beforedir}" ]]; then
-  rm -r "${beforedir}"
+if [[ -z "${key}" || -z "${value}" || -z "${filename}" ]]; then
+  
 fi
 
-mkdir -p "${todaydir}"
 
-. "${bindir}"/phonemain.sh 2>&1 | tee "${phonelog}"
+
+
+
+
+
+
+
