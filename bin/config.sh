1e48d53dc22b2bbe1a9d45dab593866681a791b3
qinweijunlin <qinweijunlin@qq.com>
2019-04-07 05:34:29 +0800

    发布

--- bin/config.sh
+++ bin/config.sh
@@ -5,26 +5,28 @@ set -euo pipefail
 
 # USAGE: config.sh 键 值 文件
 
-readonly E_MISSING_OPERAND=1
-readonly E_NOT_FOUND=2
-
-err() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
-}
+# 该命令使用前必须定义默认值
+# 允许键值对的值使用正则
+# 允许文件不存在
+# 允许键值对未定义
 
 readonly key="$1"
 readonly value="$2"
 readonly filename="$3"
 
-if [[ -z "${key}" || -z "${value}" || -z "${filename}" ]]; then
-  
+if [[ -s "${filename}" ]]; then
+
+sed -i 's/\r$//g'
+  while read line; do
+    
+    if echo "${line}" | grep -iP "^[ \t]*${key}[ \t]*=[ \t\"\']*${value}[ \t\"\']*$"; then
+      
+      line="$(echo "${line}" | sed 's/^[ \t]*//g' | sed 's/[ \t]*$//g')"
+      line="$(echo "${line}" | sed 's/[ \t]*=[ \t]*//g')"
+      line="$(echo "${line}" | sed 's///g' | sed 's///g')"
+      line="$(echo "${line}" | sed 's///g' | sed 's///g')"
+      
+    fi
+    
+  done < "${filename}"
 fi
-
-
-
-
-
-
-
-
-
