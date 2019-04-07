225f7f9c3ab54f2b35450b303903174f4f741dff
qinweijunlin <qinweijunlin@qq.com>
2019-04-07 13:19:35 +0800

    发布

--- lib/filedir.sh
+++ lib/filedir.sh
@@ -1,36 +1,21 @@
 #!/bin/bash
 #
-# 获取配置文件的值
+# 文件与目录相关函数
 set -euo pipefail
 
-# USAGE: config.sh 键 值 文件
+###################################################################################################
+#
+#       filedir::config
+#
+###################################################################################################
+err() {
+  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
+}
 
-# 该命令使用前必须定义默认值
-# 允许键值对的值使用正则
-# 允许文件不存在
-# 允许键值对未定义
+#   功能: 
+#   使用: 
+#   参数: 
+# 返回值: 
+filedir::config() {
 
-readonly key="$1"
-readonly value="$2"
-readonly filename="$3"
-
-if [[ -s "${filename}" ]]; then
-  sed -i 's/\r$//g' "${filename}"
-  
-  while read line; do
-    if echo "${line}" | grep -iP "^[ \t]*${key}[ \t]*=[ \t\"\']*${value}[ \t\"\']*$"; then
-      
-      line="$(echo "${line}" | sed 's/^[ \t]*//g' | sed 's/[ \t]*$//g')"
-      line="$(echo "${line}" | sed 's/[ \t]*=[ \t]*/=/g')"
-      line="$(echo "${line}" | sed 's/"[ \t]*/"/g' | sed 's/[ \t]*"/"/g')"
-      line="$(echo "${line}" | sed "s/'[ \t]*/'/g" | sed "s/[ \t]*'/'/g")"
-      
-      line_key="$(echo "${line%=*}" | tr '[A-Z]' '[a-z]')"
-      line_value="$(echo "${line#*=}")"
-      line="${line_key}=${line_value}"
-      
-      export "${line}"
-      
-    fi
-  done < "${filename}"
-fi
+}
