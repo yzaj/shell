f16597893205c25218edfd3e6cc2ce1dac70534d
qinweijunlin <qinweijunlin@qq.com>
2019-04-07 13:34:35 +0800

    发布

--- lib/filedir.sh
+++ lib/filedir.sh
@@ -17,5 +17,29 @@ err() {
 #   参数: 
 # 返回值: 
 filedir::config() {
-
+  local key="$1"
+  local value="$2"
+  local file="$3"
+  local line
+  
+  if [[ -s "${file}" ]]; then
+    sed -i 's/\r$//g' "${file}"
+    
+    while read line; do
+      if echo "${line}" | grep -iP "^[ \t]*${key}[ \t]*=[ \t\"\']*${value}[ \t\"\']*$"; then
+        
+        line="$(echo "${line}" | sed 's/^[ \t]*//g' | sed 's/[ \t]*$//g')"
+        line="$(echo "${line}" | sed 's/[ \t]*=[ \t]*/=/g')"
+        line="$(echo "${line}" | sed 's/"[ \t]*/"/g' | sed 's/[ \t]*"/"/g')"
+        line="$(echo "${line}" | sed "s/'[ \t]*/'/g" | sed "s/[ \t]*'/'/g")"
+        
+        key="$(echo "${line%=*}" | tr '[A-Z]' '[a-z]')"
+        value="$(echo "${line#*=}")"
+        line="${key}=${value}"
+        
+        export "${line}"
+        
+      fi
+    done < "${file}"
+  fi
 }
