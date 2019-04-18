81a19a462fb87d29fa23f42b387d86218dfff6b6
qinweijunlin <qinweijunlin@qq.com>
2019-04-18 19:49:24 +0800

    发布

--- bin/sed.sh
+++ bin/sed.sh
@@ -3,27 +3,23 @@
 # 任意仓库的卸载脚本
 set -euo pipefail
 
-# 修改 REPO 的值, 即可
-readonly REPO='shell'
-readonly MINTTY='Cygwin-Terminal.ico'
-readonly E_USE_MINTTY=1
 
-err() {
-  echo "[$(date +'%Y-%m-%dT%H:%M:%S%z')]: $@" >&2
-}
 
-readonly repodir="/yzaj/${REPO}"
-readonly src="/etc/profile.d/yzaj-${REPO}.sh"
 
-if [[ ! -f "/${MINTTY}" ]]; then
-  err "uninstall.sh: please use cygwin to execute"
-  exit "${E_USE_MINTTY}"
-fi
 
-if [[ -d "${repodir}" ]]; then
-  rm -r "${repodir}"
-fi
 
-if [[ -f "${src}" ]]; then
-  rm "${src}"
-fi
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
